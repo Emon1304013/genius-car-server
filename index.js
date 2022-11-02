def56ef103ff04aeb4639e412d6842b71445f163
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-03 01:29:57 +0600

    Api created for getting single service data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -10,10 +10,38 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ph4ajav.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try{
+        const servicesCollection = client.db('geniusCar').collection('services');
 
+        // Get Services data from database using this api 
 
+        app.get('/services',async(req,res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // Finding Single service from database 
+
+        app.get('/services/:id',async(req,res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await servicesCollection.findOne(query);
+            // const service = await cursor.toArray();
+            res.send(service);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
